c7bf9dd50804c7c04e0da0208d27a6905e7e0e2f
sunilpowar5 <sunilpowar523@gmail.com>
2024-11-21 17:16:43 +0530

    EDA and Model Training

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,7 @@
 import sys
-import logging
+from logger import logging
+# sys.path.append('E:/Machine Learning Projects/mlproject')
+# from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
