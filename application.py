51cab5f1eea8d15bc84c36c40ffc4dc058ffb927
sunilpowar5 <sunilpowar523@gmail.com>
2024-11-28 12:22:32 +0530

    Final Commit

--- application.py
+++ application.py
@@ -41,4 +41,4 @@ def predict_datapoint():
         return render_template('home.html',results=np.round(results[0],3))
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0',debug=True)
+    app.run(host='0.0.0.0')
